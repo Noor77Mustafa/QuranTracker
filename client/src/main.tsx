ea1a9876ef56a0388134fcd850cc5c2f1ada13da
Noor77Mustafa <97814356+Noor77Mustafa@users.noreply.github.com>
2025-07-31 22:34:24 -0400

    build: compile service worker and use manifest

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -9,7 +9,7 @@ import { queryClient } from "./lib/queryClient";
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
-      .register("/src/service-worker.ts")
+      .register("/service-worker.js")
       .then((registration) => {
         console.log("Service Worker registered with scope:", registration.scope);
       })
